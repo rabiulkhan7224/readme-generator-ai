984c4539a258b4fb1eb2b0d177b55a386c4a430f
rabiulkhan7224 <mdrabiulkhanbabo@gmail.com>
2025-04-27 13:10:43 +0600

    error exit

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -30,6 +30,8 @@ export default function HeroSection() {
         <>
             <HeroHeader />
 
+
+
             <main className="overflow-hidden">
                 <div
                     aria-hidden
